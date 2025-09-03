9812d19013f33367664e44180cdc9a05ebd19e18
LGNacionales <nacionaleslarrygabriel@gmail.com>
2025-09-03 18:12:40 +0800

    Update tech_extractor.py

--- utils/tech_extractor.py
+++ utils/tech_extractor.py
@@ -11,12 +11,12 @@ def extract_technologies(job_text):
     print(f"DEBUG - Input text: '{job_text[:200]}...'" if len(job_text) > 200 else f"DEBUG - Input text: '{job_text}'")
     tech_keywords = [
         # Programming Languages
-        'python', 'java', 'javascript', 'typescript', 'c++', 'c#', 'php', 'ruby', 'rust', 'swift', 'kotlin', 'scala', 'r', 'javaee'
+        'python', 'java', 'javascript', 'typescript', 'c++', 'c#', 'php', 'ruby', 'rust', 'swift', 'kotlin', 'scala', 'r', 'javaee', 'go',
         # Web Technologies
-        'html', 'css', 'react', 'angular', 'vue', 'node.js', 'express', 'django', 'flask', 'spring', 'laravel', 'html5', 'css3','handlebars', 'nuxt','jquery','asp.net','bootsrap','blazor','razor','zend',
-        'razor','laminas','drupal','twig','sass','scss','graphql',
+        'html', 'css', 'react', 'angular', 'vue', 'node', 'express.js', 'django', 'flask', 'spring', 'laravel', 'html5', 'css3','handlebars', 'nuxt','jquery','asp.net','bootstrap','blazor','razor','zend',
+        'laminas','drupal','twig','sass','scss','graphql',
         # Databases & Data Technologies
-        'sql', 'mysql', 'postgresql', 'mongodb', 'redis', 'elasticsearch', 'sqlite', 'oracle', 'sql server', 'nosql', 'hadoop', 'spark', 'etl', 'databricks',
+        'sql', 'mysql', 'postgresql', 'mongodb', 'redis', 'elasticsearch', 'sqlite', 'oracle', 'sql server', 'nosql', 'hadoop', 'spark', 'etl', 'databricks','streamlit'
         # Cloud & DevOps
         'aws', 'azure', 'gcp', 'google cloud', 'docker', 'kubernetes', 'jenkins', 'terraform', 'ansible', 'iis', 
         # Data & Analytics
@@ -51,6 +51,51 @@ def extract_technologies(job_text):
             if any(re.search(pattern, job_text_lower) for pattern in r_patterns):
                 found_technologies.append('R')
         
+        # Special handling for Scala programming language
+        elif tech_lower == 'scala':
+            scala_patterns = [
+                r'\bscala\s+programming\b', r'\bscala\s+language\b', r'\bscala\s+developer\b',
+                r'\bscala\s+engineer\b', r'\bscala\s+development\b', r'\busing\s+scala\b', r'\bwith\s+scala\b',
+                r'\bin\s+scala\b', r'\bscala\s+code\b', r'\bscala\s+application\b', r'\bscala\s+service\b',
+                r'\bknowledge\s+of\s+scala\b', r'\bexperience\s+with\s+scala\b', r'\bproficient\s+in\s+scala\b',
+                r'\bscala\s+and\s+java\b', r'\bjava\s+and\s+scala\b', r'\bscala\s+or\s+java\b',
+                r'\bjava\s+or\s+scala\b', r'\bscala\s*[,/]\s*java\b', r'\bjava\s*[,/]\s*scala\b',
+                r'\bscala\s+skills\b', r'\bscala\s+expertise\b', r'\bscala\s+background\b'
+            ]
+            
+            if any(re.search(pattern, job_text_lower) for pattern in scala_patterns):
+                found_technologies.append('Scala')
+        
+        # Special handling for Go programming language
+        elif tech_lower == 'go':
+            go_patterns = [
+                r'\bgo\s+programming\b', r'\bgo\s+language\b', r'\bgo\s+developer\b',
+                r'\bgo\s+engineer\b', r'\bgolang\b', r'\busing\s+go\b', r'\bwith\s+go\b',
+                r'\bin\s+go\b', r'\bgo\s+code\b', r'\bgo\s+application\b', r'\bgo\s+service\b',
+                r'\bknowledge\s+of\s+go\b', r'\bexperience\s+with\s+go\b', r'\bproficient\s+in\s+go\b',
+                r'\bgo\s+and\s+python\b', r'\bpython\s+and\s+go\b', r'\bgo\s+or\s+python\b',
+                r'\bpython\s+or\s+go\b', r'\bgo\s*[,/]\s*python\b', r'\bpython\s*[,/]\s*go\b'
+            ]
+            
+            if any(re.search(pattern, job_text_lower) for pattern in go_patterns):
+                found_technologies.append('Go')
+        
+        # Special handling for iOS mobile platform
+        elif tech_lower == 'ios':
+            ios_patterns = [
+                r'\bios\s+app\b', r'\bios\s+application\b', r'\bios\s+development\b', r'\bios\s+developer\b',
+                r'\bios\s+mobile\b', r'\bios\s+sdk\b', r'\bios\s+platform\b', r'\bios\s+device\b',
+                r'\bnative\s+ios\b', r'\bmobile\s+ios\b', r'\bswift\s+ios\b', r'\bios\s+swift\b',
+                r'\bios\s+and\s+android\b', r'\bandroid\s+and\s+ios\b', r'\bios\s+or\s+android\b',
+                r'\bandroid\s+or\s+ios\b', r'\bios\s*[,/]\s*android\b', r'\bandroid\s*[,/]\s*ios\b',
+                r'\bios\s+programming\b', r'\bios\s+engineer\b', r'\busing\s+ios\b', r'\bwith\s+ios\b',
+                r'\bknowledge\s+of\s+ios\b', r'\bexperience\s+with\s+ios\b', r'\bproficient\s+in\s+ios\b',
+                r'\bios\s+skills\b', r'\bios\s+expertise\b', r'\bxcode\s+ios\b', r'\bios\s+xcode\b'
+            ]
+            
+            if any(re.search(pattern, job_text_lower) for pattern in ios_patterns):
+                found_technologies.append('iOS')
+        
         # Special handling for Excel
         elif tech_lower == 'excel':
             excel_patterns = [
@@ -66,20 +111,6 @@ def extract_technologies(job_text):
             
             if any(re.search(pattern, job_text_lower) for pattern in excel_patterns):
                 found_technologies.append('Excel')
-        
-        # Special handling for Go programming language
-        elif tech_lower == 'go':
-            go_patterns = [
-                r'\bgo\s+programming\b', r'\bgo\s+language\b', r'\bgo\s+developer\b',
-                r'\bgo\s+engineer\b', r'\bgolang\b', r'\busing\s+go\b', r'\bwith\s+go\b',
-                r'\bin\s+go\b', r'\bgo\s+code\b', r'\bgo\s+application\b', r'\bgo\s+service\b',
-                r'\bknowledge\s+of\s+go\b', r'\bexperience\s+with\s+go\b', r'\bproficient\s+in\s+go\b',
-                r'\bgo\s+and\s+python\b', r'\bpython\s+and\s+go\b', r'\bgo\s+or\s+python\b',
-                r'\bpython\s+or\s+go\b', r'\bgo\s*[,/]\s*python\b', r'\bpython\s*[,/]\s*go\b'
-            ]
-            
-            if any(re.search(pattern, job_text_lower) for pattern in go_patterns):
-                found_technologies.append('Go')
         
         # Regular matching for multi-character technologies
         else:
